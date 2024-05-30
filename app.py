13942623f23fe930b7925232860d95c72be93f0e
Jesse <sohnjesse@gmail.com>
2024-05-29 21:40:27 -0700

    commit

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "hello world"
+def index():
+    return render_template('index.html')
+
+@app.route('/home/<int:id>')
+def hello(id):
+    return "Hello, " + str(id)
+
+
+@app.route('/onlyget', methods = ['POST', 'GET'])
+def get_req():
+    return 'You can only get this webpage.'
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
+
