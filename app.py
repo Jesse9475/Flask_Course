613f2e2478d8960f6fdac442732a4a3243968ab1
Jesse <sohnjesse@gmail.com>
2024-06-11 20:03:03 -0700

    commit

--- app.py
+++ app.py
@@ -6,14 +6,36 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/home/<int:id>')
-def hello(id):
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'This is the content of post 1. SIGMA',
+        'author': 'Jesse'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'This is the content of post 2. RiZZLER'
+    }
+]
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts = all_posts)
+
+@app.route('/home')
+@app.route('/home/<id>')
+def hello(id=None):
     return "Hello, " + str(id)
 
 
 @app.route('/onlyget', methods = ['POST', 'GET'])
 def get_req():
     return 'You can only get this webpage.'
+
+@app.route('/name/<username>')
+def get_username(username = ''):
+    return render_template('index.html', username = username)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
