d796e177f5f0889c506d1eda0f7b32a79af2d7d4
Jesse9475 <sohnjesse@gmail.com>
2025-01-03 21:52:30 -0800

    changed couple of files

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
-# make comparing logic for album mesh
-# generate random albums (from list of albums)
-# very basic buttons
-# 
+
 
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-top_spotify_albums = [ "Un Verano Sin Ti - Bad Bunny", "÷ (Divide) - Ed Sheeran", "Starboy - The Weeknd",
-                      "Hollywood's Bleeding - Post Malone", "Sour - Olivia Rodrigo", "After Hours - The Weekend",
-                      "Dua Lipa - Dua Lipa", "Future Nostalgia - Dua Lipa", "Beerbongs & Bentleys - Post Malone",
-                      "When We All Fall Asleep, Where Do We Go? - Billie Eilish" ]
-top_spotify_albums_reversed = top_spotify_albums[::-1]
-a = 0
-b = 0
-count_list = [0] * len(top_spotify_albums)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
@@ -29,34 +17,6 @@ class BlogPost(db.Model): # Model for the database
     
     def __repr__(self): #will print whenever a new blog post is created (the constructor)
         return 'Blog post ' + str(self.id)
-
-
-@app.route('/album_mesh', methods = ['GET', 'POST'])
-def album_mesh():
-    if a or b >= len(top_spotify_albums):
-        return redirect('/album_mesh/results')
-    else:
-        return render_template('logic.html', a = a, b = b, song_a = top_spotify_albums[a], song_b = top_spotify_albums_reversed[b])
-
-@app.route('/album_mesh/results', methods = ['POST'])
-def album_mesh_results(count_list):
-    top_songs_sorted = {}
-    for i in range(0,len(top_spotify_albums)):
-        top_songs_sorted[top_spotify_albums[i]] = count_list[i]
-    top_songs_sorted = dict(sorted(top_songs_sorted.items(), key=lambda item: item[1], reverse=True))
-    return render_template('results.html', top_spotify_albums, top_songs_sorted)
-
-@app.route('/album_mesh/song_1/<int:a>', methods = ['GET', 'POST'])
-def album_mesh_song_1(a = a, b =b , count_list = count_list):
-    count_list[a] += 1
-    b += 1
-    return redirect('/album_mesh')
-
-@app.route('/album_mesh/song_2/<int:b>', methods = ['GET', 'POST'])
-def album_mesh_song_2(a = a, b =b , count_list = count_list):
-    count_list[-(b+1)] += 1
-    a += 1
-    return redirect('/album_mesh')
 
 @app.route('/')
 def index():
